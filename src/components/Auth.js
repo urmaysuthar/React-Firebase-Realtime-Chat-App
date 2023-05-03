24f2c37360c512b1ad869c7d328a9e85a2599675
urmaysuthar <urmay.suthar0100@gmail.com>
2023-05-02 22:50:41 -0400

    Added CSS Styles

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,11 +4,15 @@ import { signInWithPopup } from "firebase/auth";
 import Cookies from 'universal-cookie'
 const cookies = new Cookies();
 
-export const Auth = () =>{
+export const Auth = (props) =>{
+
+    const {setIsAuth} = props;
+
     const signInWithGoogle = async () => {
         try{
        const result = await signInWithPopup(auth, provider);
        cookies.set("auth-token", result.user.refreshToken);
+       setIsAuth(true);
         } catch(err){
             console.error(err);
         }
